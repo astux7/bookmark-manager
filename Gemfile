cfdcc482f885f67f6a1b9fb2b4e69a76e4401d78
astux7 <Asta.bevainyte@gmail.com>
2013-12-18 12:37:57 +0000

    first chapter finito

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+ruby "2.0.0"
+gem 'sinatra' # our web framework
+gem 'shotgun' # for live code reloading
+gem 'sinatra-partial'  #for split erb files to parts
+gem 'rack-flash3' #for showing flash messages for user
+gem 'data_mapper'  #data mapper with sinatra and postgress
+gem 'dm-postgres-adapter' #adapter to work with postgress
+gem 'capybara' #testing client side
+gem 'database_cleaner' #to clean the database after the test
+group :development, :test do
+  gem "rspec"  #testing server side
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,32 @@
+
+require 'capybara/rspec'
+require 'sinatra'
+require 'database_cleaner'
+# Remember environment variables from week 1?
+ENV["RACK_ENV"] = 'test' # because we need to know what database to work with
+
+# this needs to be after ENV["RACK_ENV"] = 'test' 
+# because the server needs to know
+# what environment it's running it: test or development. 
+# The environment determines what database to use.
+require './server' 
+
+Capybara.app = Sinatra::Application
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  
+end
+
