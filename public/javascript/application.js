10e311f9b4999362ee429cdfec6ca7350578e7e7
astux7 <Asta.bevainyte@gmail.com>
2014-01-13 17:57:40 +0000

    added add link link in index

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -15,10 +15,41 @@ $(".star.solid").click(function(event) {
   $(link).data("favourited", !favourited);    
   // perform the animation
   $(this).animate({opacity: newOpacity}, 1000);
+  showLinkFavouritedNotice(link);
 });
+
+}
+
+function showLinkFavouritedNotice(link) {
+  var favourited = !!$(link).data("favourited");
+  // get the text of the .title element
+  // that we find among the children of link
+  var name = $(link).find('.title').text();
+  var message = favourited ?
+                name + " was added to favourites" :
+                name + " was removed from favourites";
+  var flash = $("<div></div>").addClass('flash').addClass('notice').html(message);
+  $(flash).appendTo('#flash-container');
+  setTimeout(function() {
+    $(flash).fadeOut();
+  }, 3000);
+};
+
+function prepareNewLinkHandler() {
+  $('.add-link').click(function(event) {
+   $.get($(this).attr("href"), function(data) {
+	  if ($('#ajax-form').length == 0) {
+	    $("#container").prepend("<div id='ajax-form'></div>");
+	  }
+	  $('#container #ajax-form').html(data);
+	});
+    event.preventDefault();
+  });
 }
 
 $(function() {
   animateLinks();
   addFavouritesHandler();
+  prepareNewLinkHandler();
+
 })
