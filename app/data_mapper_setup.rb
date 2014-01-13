1f392626a722257711fbd2a7e5249aee34cf28ac
astux7 <Asta.bevainyte@gmail.com>
2014-01-13 16:57:04 +0000

    added css and js link animation and fav

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,7 +2,7 @@ env = ENV["RACK_ENV"] || "development"
 
 # we're telling datamapper to use a postgres database on localhost. The name will
 # be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
-DataMapper::Logger.new("log.txt", :debug)
+#DataMapper::Logger.new("log.txt", :debug)
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 
---
+++ public/javascript/application.js
@@ -0,0 +1,24 @@
+function animateLinks() {
+  $('.link').show(1000);
+}
+
+function addFavouritesHandler() {
+$(".star.solid").click(function(event) {  
+  // get the link this star belongs to
+  var link = $(this).parent();
+  // get a boolean value for 'favourited'
+  // double negation casts any value to boolean
+  var favourited = !!$(link).data("favourited");
+  // decide what the opacity is going to be
+  var newOpacity = favourited ? 0 : 1;
+  // toggle the 'favourited' variable on the link element
+  $(link).data("favourited", !favourited);    
+  // perform the animation
+  $(this).animate({opacity: newOpacity}, 1000);
+});
+}
+
+$(function() {
+  animateLinks();
+  addFavouritesHandler();
+})
